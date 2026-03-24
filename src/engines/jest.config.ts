55c4b886b1d01dc6e7f88b177502e6a06c0df25f
allanfoppa <allanfoppa@hotmail.com>
2026-03-24 15:37:12 -0300

    Fix prepare command to AI Text Summarize App

---
+++ src/engines/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  // Use the env var we set in Step 5 of the YAML
+  rootDir: `../../apps/${process.env.PROJECT_ID_ENV}/${process.env.PROJECT_ROOT_PATH || ""}`,
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  // Ensure we can resolve modules from the cloned app's folder
+  moduleDirectories: ["node_modules", `<rootDir>/node_modules`],
+};
