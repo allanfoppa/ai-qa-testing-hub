2220dda22de8ae564f1652e34bd922a496e642a9
allanfoppa <allanfoppa@hotmail.com>
2026-04-02 17:06:12 -0300

    Remove coverage

--- src/engines/jest/jest.config.cjs
+++ src/engines/jest/jest.config.cjs
@@ -1,15 +1,10 @@
 const path = require("path");
 const projectRoot = path.resolve(__dirname, "../../../");
 
-const suiteName = process.env.SUITE_NAME || "all-suites";
-const suiteVersion = process.env.SUITE_VERSION || "latest";
-
 module.exports = {
   testEnvironment: "node",
 
   rootDir: path.resolve(process.cwd()),
-
-  extensionsToTreatAsEsm: [".ts"],
 
   testMatch: [`<rootDir>/**/${process.env.SUITE_PATH || "*"}.spec.{ts,js}`],
 
@@ -22,23 +17,9 @@ module.exports = {
     "^@config/(.*)$": path.join(projectRoot, "config/$1"),
   },
 
-  collectCoverage: true,
+  collectCoverage: false, // Here we test a black-box QA/testing hub, and Jest coverage is a white-box tool.
 
-  coverageDirectory: path.resolve(
-    projectRoot,
-    "src/engines/jest/coverage",
-    suiteName,
-    suiteVersion,
-  ),
+  modulePaths: [projectRoot],
 
   moduleFileExtensions: ["js", "ts", "json", "node"],
-
-  transform: {
-    "^.+\\.(t|j)s$": [
-      path.join(projectRoot, "node_modules", "ts-jest"),
-      {
-        useESM: true,
-      },
-    ],
-  },
 };
